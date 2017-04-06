e9aa90832d0e8173768db28973c584106b4eae23
Andrew Labrecque <andrew.labrecque@buildium.com>
2017-04-06 13:07:29 -0400

    Script to build project for deployment.

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Copy the `config-template.js` to `config.js` and fill in the following parameter
 * A Google API Client ID configured for use with Google Sign In and the Google Calendar API.
 
 Run a web server to host files from the root of the repo.
+
+## Deployment
+
+Run the `./bulid.sh` script and compress the contents of the `build/` directory and upload to AWS.
---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copies files to build/ for archive and upload to server.
+# Updates occurences of REPLACE_VERSION in files with a unique UUID for the version.
+
+rm -rf build
+mkdir build
+cp -r images build/images
+cp -r scripts build/scripts
+cp -r styles build/styles
+cp -r config.js build/config.js
+cp -r index.html build/index.html
+cp -r service-worker.js build/service-worker.js
+cp -r manifest.json build/manifest.json
+cp -r favicon.ico build/favicon.ico
+
+version=$(uuidgen)
+
+sed -i.bak "s/REPLACE_VERSION/$version/g" build/index.html
+rm build/index.html.bak
+sed -i.bak "s/REPLACE_VERSION/$version/g" build/service-worker.js
+rm build/service-worker.js.bak
+
+echo "Now compress the contents of ./build/ and deploy to AWS"
